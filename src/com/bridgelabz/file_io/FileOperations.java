ffa8826b283000d9d8759a33c75895092593068f
Akash Das <akashd595@gmail.com>
2022-08-08 23:11:44 +0530

    ADDED: file operations

---
+++ src/com/bridgelabz/file_io/FileOperations.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.file_io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileOperations {//E:\
+
+    public static void main(String[] args) {
+        try {
+
+            File file = new File("E:\\aug08.txt");
+            file.createNewFile();
+
+            System.out.println("file exists or not : "+file.exists());
+
+            file.delete();
+            System.out.println("file exists or not : "+file.exists());
+
+            File file1 = new File("E:\\aug08\\");
+
+            file1.mkdir();
+
+            Files.list(Path.of("E:\\RFP-Batch\\")).filter(Files::isRegularFile).forEach(System.out::println);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
