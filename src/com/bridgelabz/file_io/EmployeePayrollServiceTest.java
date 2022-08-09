d714226cfa2111ad98570e9355e78907092d3e81
Akash Das <akashd595@gmail.com>
2022-08-09 21:51:14 +0530

    ADDED: uc5 Print employee payroll service in a file

---
+++ src/com/bridgelabz/file_io/EmployeePayrollServiceTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.file_io;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class EmployeePayrollServiceTest {
+
+    @Test
+
+    public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+
+        EmployeePayrollData[] arrayOfEmp = { new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
+
+        employeePayrollService.writeEmployeeData(EmployeePayrollService.IOService.FILE_IO);
+
+
+        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        assertEquals(3, entries);
+    }
+}
